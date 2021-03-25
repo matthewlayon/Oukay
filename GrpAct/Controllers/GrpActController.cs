43ac21efe1bf31dcbc104d856df909091a071f49
Jan Matthew Layon <11910426@benilde.edu.ph>
2021-03-25 21:10:52 +0800

    Login and Shop not yet functional

---
+++ GrpAct/Controllers/GrpActController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GrpAct.Controllers
+{
+    public class GrpActController : Controller
+    {
+        // GET: Home
+        public ActionResult Home()
+        {
+            return View();
+        }
+        public ActionResult Login()
+        {
+            return View();
+        }
+        public ActionResult Checkout()
+        {
+            return View();
+        }
+        public ActionResult Cart()
+        {
+            return View();
+        }
+    }
+}
